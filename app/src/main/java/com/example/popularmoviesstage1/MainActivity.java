8dd5452384d20e59b7f6a6d36922475311e05e07
mahmood <mahmood.ghubn@gmail.com>
2019-08-23 01:19:05 +0300

    requesting more than one page

--- app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesstage1/MainActivity.java
@@ -9,6 +9,10 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.popularmoviesstage1.model.Film;
 import com.example.popularmoviesstage1.utilities.NetworkUtils;
@@ -20,6 +24,7 @@ import com.example.popularmoviesstage1.FilmAdapter.FilmAdapterOnClickHandler;
 public class MainActivity extends AppCompatActivity implements FilmAdapterOnClickHandler {
 
     private FilmAdapter mAdapter;
+    private static int pageNumber =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,27 @@ public class MainActivity extends AppCompatActivity implements FilmAdapterOnClic
         mRecyclerView.setHasFixedSize(true);
         mAdapter = new FilmAdapter(this);
 
-        new FetchFilmData().execute(NetworkUtils.MOST_POPULAR_MOVIES_API);
+        new FetchFilmData().execute("1",NetworkUtils.POPULARITY);
+        //TODO take the value of sort_by from menu
+//        new FetchFilmData().execute(NetworkUtils.MOST_POPULAR_MOVIES_API);
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    //TODO take sort by from another place
+                    pageNumber++;
+                            new FetchFilmData().execute(Integer.toString(pageNumber) ,NetworkUtils.POPULARITY);
+
+                            Log.i("mainAc",Integer.toString(pageNumber));
+
+//                    Toast.makeText(MainActivity.this, "Last", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
     }
 
     @Override
@@ -53,8 +77,9 @@ public class MainActivity extends AppCompatActivity implements FilmAdapterOnClic
                 return null;
             }
 
-            String filmsUrl = params[0];
-            URL filmsRequestUrl = NetworkUtils.createUrl(filmsUrl);
+            String pageNumber = params[0];
+            String sort_by = params[1];
+            URL filmsRequestUrl = NetworkUtils.createUrl(pageNumber,sort_by);
 
             try {
                 String jsonFilmsResponse = NetworkUtils
@@ -80,9 +105,41 @@ public class MainActivity extends AppCompatActivity implements FilmAdapterOnClic
         protected void onPostExecute(String[] strings) {
             if (strings != null){
                 mAdapter.setFilmData(strings);
+//                mAdapter.
             }else {
                 //TODO show error message
             }
         }
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.menu, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.most_popular) {
+            mAdapter.setFilmData(null);
+//            setUrl();
+            return true;
+        }else if (id == R.id.highest_rated){
+            mAdapter.setFilmData(null);
+//            setUrl();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void setUrl(String Url){
+
+    }
+
 }
